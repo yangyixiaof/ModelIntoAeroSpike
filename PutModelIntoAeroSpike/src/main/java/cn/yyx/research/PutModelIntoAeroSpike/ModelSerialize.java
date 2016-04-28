3e6fa7aa939f78557d3a2457797c7dbd5de1fea0
yyx <yyx@ubuntu>
2016-04-27 20:23:59 -0700

    small change.

--- PutModelIntoAeroSpike/src/main/java/cn/yyx/research/PutModelIntoAeroSpike/ModelSerialize.java
+++ PutModelIntoAeroSpike/src/main/java/cn/yyx/research/PutModelIntoAeroSpike/ModelSerialize.java
@@ -26,9 +26,22 @@ public class ModelSerialize {
 	
 	public void StartPutCountModelIntoAeroSpike(int order) throws Exception {
 		{
+			// raw one sentence parse block.
+			// ComplexParser.GetSentence("A@@C0?0*=this.frequencyX");
+			// ComplexParser.GetSentence("PeE@+.5");
+			// ComplexParser.GetSentence("Q@ACTION_SIZE.Action]");
+			// System.err.println("one sentence parse block over.");
+			// System.exit(1);
+		}
+		
+		{
 			OrderModelIterator omi = new Order1ModelIterator();
 			omi.IterateFile();
 		}
+		
+		System.err.println("Currently Stop Here, just test order1 to check parse functionality.");
+		System.exit(1);
+		
 		for (int i=1;i<order;i++)
 		{
 			OrderModelIterator omi = new OrderModelIterator(i+1);
