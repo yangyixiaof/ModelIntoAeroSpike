b08ac30c679cdd0db8c420da4389a5effc239959
yyx <yyx@ubuntu>
2019-08-19 04:33:36 -0700

    small change.

--- PutModelIntoAeroSpike/src/test/java/cn/yyx/research/PutModelIntoAeroSpike/AppTest.java
+++ PutModelIntoAeroSpike/src/test/java/cn/yyx/research/PutModelIntoAeroSpike/AppTest.java
@@ -1,38 +1,15 @@
 package cn.yyx.research.PutModelIntoAeroSpike;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
+public class AppTest {
+    
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static void suite() {
+    	System.out.println("Ha");
     }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+    
 }
